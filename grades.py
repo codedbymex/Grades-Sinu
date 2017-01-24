0e87f6d4f706ee478e5304408f6176a1857b2705
Norbert Bota <botanorbert1@gmail.com>
2017-01-24 17:29:49 +0200

    Update grades.py

--- grades.py
+++ grades.py
@@ -3,9 +3,9 @@ import requests
 
 base_url = "https://sinu.utcluj.ro/Note_up/"
 
-UTILIZATOR = "UTILIZATOR"
-PAROLA = "PAROLA"
-
+UTILIZATOR = "UTILIZATOR" #Username here
+PAROLA = "PAROLA"         #Password here
+ngrades = 10              #Number of grades here
 
 
 payload1 = {'hidSelfSubmit':'default.asp',
@@ -23,7 +23,7 @@ soup = BeautifulSoup(r1.content, "lxml")
 table = soup.find("table", { "class" : "table" })
 
 def noteall():
-    for row in table.findAll("tr")[1:10]:
+    for row in table.findAll("tr")[1:ngrades]:
         cells = row.findAll("td")
         if len(cells) > 4:
             note = "\n" + cells[0].text + cells[3].text
