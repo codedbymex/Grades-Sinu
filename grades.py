5371806c60946e596429fd18f9dccb04888f0d9e
Norbert Bota <botanorbert1@gmail.com>
2017-01-24 17:19:15 +0200

    Update grades.py

--- grades.py
+++ grades.py
@@ -1,11 +1,11 @@
 from bs4 import BeautifulSoup
-from tabulate import tabulate
 import requests
 
 base_url = "https://sinu.utcluj.ro/Note_up/"
 
 UTILIZATOR = "UTILIZATOR"
 PAROLA = "PAROLA"
+ngrades = "n" #number of grades
 
 
 payload1 = {'hidSelfSubmit':'default.asp',
@@ -23,7 +23,7 @@ soup = BeautifulSoup(r1.content, "lxml")
 table = soup.find("table", { "class" : "table" })
 
 def noteall():
-    for row in table.findAll("tr")[1:10]:
+    for row in table.findAll("tr")[1:ngrades]:
         cells = row.findAll("td")
         if len(cells) > 4:
             note = "\n" + cells[0].text + cells[3].text
